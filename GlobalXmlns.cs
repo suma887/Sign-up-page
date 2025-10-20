232aa88070974b4ece217cc50e6f2f86bfb64dd8
suma887 <nannapln@mail.uc.edu>
2025-10-19 19:19:34 -0700

    Delete GlobalXmlns.cs

--- GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Signup2")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Signup2.Pages")]
